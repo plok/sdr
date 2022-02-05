2e9af5656f4ffaf1fbf22f579411ac057b8c848a
Jorrit Everts <jorrit@baseflow.com>
2022-02-05 19:18:41 +0100

    Add GTK4 dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+use gtk4 as gtk;
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder()
+        .application_id("org.plok.sdr")
+        .build();
+
+    app.connect_activate(|app| {
+        // We create the main window.
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .default_width(1920)
+            .default_height(1080)
+            .title("Hello, World!")
+            .build();
+
+        // Show the window.
+        window.show();
+    });
+
+    app.run();
 }
